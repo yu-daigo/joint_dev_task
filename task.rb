ac8e18edbb229ad09d92a2ec07ae0cc0b26a1e13
YU <daigoyou0707@gmail.com>
2020-04-02 21:09:53 +0900

    課題Q6　putsからpに修正。Q７〜10を回答 しました。

--- task.rb
+++ task.rb
@@ -55,7 +55,7 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-
+  array = array.map(&:to_i)
   # 以下は変更しないで下さい
   p array
 end
@@ -64,7 +64,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-
+  programming_languages.map!(&:capitalize) 
+  upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
@@ -74,14 +75,21 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+  names.each_with_index{|name, i| 
+  p "会員No.#{i+1} #{name}さん" }
 end
 
 def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-
+  foods.each{|food|
+  if food.include?("うに") 
+    p "好物です"
+  else 
+    p "まぁまぁ好きです"
+  end
+  }
 end
 
 def q11
