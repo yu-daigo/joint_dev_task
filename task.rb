6257d62d03366bd8231d1613e84fccaf7cb6b832
YU <daigoyou0707@gmail.com>
2020-04-05 17:33:03 +0900

    Ruby課題Q15、16を解凍しました。

--- task.rb
+++ task.rb
@@ -152,6 +152,8 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
+p data1.key?(:age) ? "OK" : "NO"
+p data2.key?(:age) ? "OK" : "NO"
 
 end
 
@@ -164,6 +166,9 @@ def q16
   ]
 
   # 以下に回答を記載
+  users.each{|user|
+  puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}歳です。"
+  }
 
 end
 
